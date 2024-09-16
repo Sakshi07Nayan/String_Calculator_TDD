40dbb3f9a5ec260a2cea98e8cc6e82f17d3aafc2
Sakshi Nayan <sakshi12princi@gmail.com>
2024-09-16 16:59:41 +0530

    Handle single number input

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -6,4 +6,8 @@ describe('String Calculator', () => {
         const calculator = new StringCalculator();
         expect(calculator.add('')).to.equal(0);
     });
+    it('should return the number itself when a single number is provided', () => {
+        const calculator = new StringCalculator();
+        expect(calculator.add('1')).to.equal(1);
+    });
 });
